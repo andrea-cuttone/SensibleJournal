2eac06d9600aa4bac772a83854890767c48ead61
Andrea Cuttone <andreacuttone@gmail.com>
2014-06-01 11:01:05 +0200

    sharedprefs will not be lost on upgrade

--- SensibleJournal/src/dk/dtu/imm/sensiblejournal2013/login/AuthActivity.java
+++ SensibleJournal/src/dk/dtu/imm/sensiblejournal2013/login/AuthActivity.java
@@ -170,7 +170,8 @@ public class AuthActivity extends Activity {
     }
     
     public static SharedPreferences getSystemPrefs(Context context) {
-		return context.getSharedPreferences(AuthActivity.class.getName() + "_system", MODE_PRIVATE);
+    	// this needs to be hardcoded as we need to reuse the sharedprefs from the 1.x version, and the class package has changed
+		return context.getSharedPreferences("dk.dtu.imm.sensible.AuthActivity_system", MODE_PRIVATE);
 	}
     
 
